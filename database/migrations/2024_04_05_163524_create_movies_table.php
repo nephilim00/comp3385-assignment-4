627576e117e0d37303f1155bb70b9c06de1f4ae2
Keithley Meade <keithleym89@gmail.com>
2024-04-05 12:48:00 -0400

    update

---
+++ database/migrations/2024_04_05_163524_create_movies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title'); // Add a column for the movie title
+            $table->text('description'); // Add a column for the movie description
+            $table->string('poster'); // Add a column for the path and filename of the uploaded poster image
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
