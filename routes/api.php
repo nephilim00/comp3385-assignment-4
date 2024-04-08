9133b94e9c483c07a164e681f4cb2f79e7b8cdc5
Keithley Meade <keithleym89@gmail.com>
2024-04-08 12:46:28 -0400

    update

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\AuthController;
 
-// Add middleware to all movie-related routes
-Route::middleware('auth:api')->group(function () {
-    Route::get('/v1/movies', [MovieController::class, 'index']);
-    Route::post('/v1/movies', [MovieController::class, 'store']);
-    // If you have more movie routes, add them here
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/v1/movies', [MovieController::class, 'index']);
+Route::post('/v1/movies', [MovieController::class, 'store']);
+Route::get('/api/v1/movies', [MovieController::class, 'index']);
 
 // Publicly accessible routes
 Route::post('/v1/login', [AuthController::class, 'login']);
